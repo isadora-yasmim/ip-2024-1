02c741e88ca1820ab22bca6439688028877f232c
isadora-yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-05-14 10:20:06 -0300

    Update 13.go

--- Lista03/13.go
+++ Lista03/13.go
@@ -1,13 +1,33 @@
 package main
-func main(){
+
+import "fmt"
+
+func main() {
 	var N int
+	maisFrequente := 0
+	maiorFrequencia := 0
+
 	fmt.Scan(&N)
+
 	if N <= 1000 && N > 1 {
 		V := make([]int, N)
-		
+
 		for i := 0; i < N; i++ {
 			fmt.Scan(&V[i])
 		}
+
+		frequencias := make(map[int]int)
+
+		for _, n := range V {
+			frequencias[n]++
+			if frequencias[n] > maiorFrequencia {
+				maiorFrequencia = frequencias[n]
+				maisFrequente = n
+			}
+		}
 	}
+
+	fmt.Printf("%d\n", maisFrequente)
+	fmt.Printf("%d\n", maiorFrequencia)
 
 }
